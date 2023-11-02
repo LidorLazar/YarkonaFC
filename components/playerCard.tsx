3103d91beb1fd61b4626f461ab69f7d6ce4d539d
LidorLazar <lidorlazar33@gmail.com>
2023-11-02 03:11:22 +0200

    fix a home page and fetch data from db user to playerCard.tsx

--- components/playerCard.tsx
+++ components/playerCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 async function getPlayerData() {
 
-    const response: any = await fetch('http://localhost:3000/api/data');
+    const response: any = await fetch('https://yarkona-fc.vercel.app/api/data');
     return response.json();
 }
 
